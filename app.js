9c5598ccdfc06e5b932212a3fdcba8f6f8b90aae
viseth89 <viseth89@gmail.com>
2018-12-20 07:12:25 -0600

    tweaked port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('it works')
 })
 
-const port = process.env.Port || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`)
