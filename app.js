2b9cc3eb74689b82bfd5c2cde59ecd4d2be77485
viseth89 <viseth89@gmail.com>
2018-12-20 07:04:39 -0600

    second commit testing ignore

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('it works')
+// })
+
+const port = process.env.Port || 5000
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
